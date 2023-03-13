f506ce2fb0f85ca6170a19a2139161dea8c700a3
jo-cassone <103076117+jo-cassone@users.noreply.github.com>
2023-03-13 17:05:55 -0300

    Delete sites/practica-ppt3 directory

--- sites/practica-ppt3/scripts.js
+++
@@ -1,6 +0,0 @@
-$('#btn-dbclick').dblclick(function(){
-    $('section').children("#caja-centro").remove();
-    $('h1').prependTo('main');
-    $('#caja-arriba').prependTo('section#ejercicio');
-    $('#caja-arriba').css("background","#000").css("color", "#fff")
-});
